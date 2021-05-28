8ed861282fcaff33c464b05b472f4e501f67aaa6
anuvratnehra <84926534+anuvratnehra@users.noreply.github.com>
2021-05-28 16:07:56 +0530

    Add files via upload

---
+++ Basic Data Types/FInd the runner-up score.py
@@ -0,0 +1,4 @@
+#Hackerank doesnot show my code that i solved it with...
+#it just shows empty editor( ˘︹˘ )
+#however it shows the challange completed ¯\_(ツ)_/¯
+#my profile https://www.hackerrank.com/anuvratnehra
